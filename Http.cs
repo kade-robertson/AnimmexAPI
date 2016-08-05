45351512df36464ac354dc991583eaa72605b8bf
Kade <kade.robertson16@gmail.com>
2016-08-04 20:22:32 -0400

    post added, removed unnecessary usings

--- Http.cs
+++ Http.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -29,6 +26,41 @@ namespace AnimmexAPI
                             responseMessage.EnsureSuccessStatusCode();
                             return new HttpResult(string.Empty, responseMessage.RequestMessage.RequestUri.ToString());
                         } else {
+                            responseMessage = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                            responseMessage.EnsureSuccessStatusCode();
+                            return new HttpResult(await responseMessage.Content.ReadAsStringAsync(), responseMessage.RequestMessage.RequestUri.ToString());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static async Task<HttpResult> DoPostAsync(string url, string referer, UserAgent ua = default(UserAgent), CookieContainer cookies = default(CookieContainer), FormUrlEncodedContent postdata = default(FormUrlEncodedContent), bool readdata = false)
+        {
+            using (var handler = new HttpClientHandler() { UseProxy = false, Proxy = null, CookieContainer = cookies })
+            {
+                using (var client = new HttpClient(handler))
+                {
+                    try
+                    {
+                        client.DefaultRequestHeaders.UserAgent.ParseAdd(ua.ToString());
+
+                        HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, url);
+                        requestMessage.Headers.Referrer = new Uri(referer);
+                        requestMessage.Content = postdata;
+
+                        HttpResponseMessage responseMessage;
+                        if (readdata)
+                        {
+                            responseMessage = await client.GetAsync(url);
+                            responseMessage.EnsureSuccessStatusCode();
+                            return new HttpResult(string.Empty, responseMessage.RequestMessage.RequestUri.ToString());
+                        }
+                        else {
                             responseMessage = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
                             responseMessage.EnsureSuccessStatusCode();
                             return new HttpResult(await responseMessage.Content.ReadAsStringAsync(), responseMessage.RequestMessage.RequestUri.ToString());
